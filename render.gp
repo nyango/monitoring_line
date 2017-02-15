6883ac3f460c8118a8da3cb06e91e7fc0b4c1fd7
nyango <nyango.tech@gmail.com>
2017-02-16 03:37:04 +0900

    add title/ track 2days

--- render.gp
+++ render.gp
@@ -3,5 +3,7 @@ set xdata time
 set timefmt "%y/%m/%d %H:%M:%S"
 set format x "%H:%M"
 
+set title "line speed"
+
 set output outputfile
 plot [][0:] datafile using 1:3 title "response time[ms]" with lines
--- render.sh
+++ render.sh
@@ -5,7 +5,7 @@ here=$(cd $(dirname $0);pwd)
 datestr=$(date +%y%m%d)
 tsvfile=$here/target/${datestr}.tsv
 output=$here/target/${datestr}.png
-cat $here/logs/speed_${datestr}.log | awk -F'[ /:]' '{print $1"/"$2"/"$3" "$4":"$5":"$6"\t"$15}' > $tsvfile
+cat $here/logs/speed_$(date --date '1 day ago' +%y%m%d).log $here/logs/speed_${datestr}.log | awk -F'[ /:]' '{print $1"/"$2"/"$3" "$4":"$5":"$6"\t"$15}' > $tsvfile
 gnuplot -e "datafile='${tsvfile}'; outputfile='${output}'" $here/render.gp
 
 # configure as you like
