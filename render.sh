64c4dce4beb94db91675d12ee9ebdb95cf0d411a
nyango <nyango.tech@gmail.com>
2017-02-16 03:28:57 +0900

    add comment

--- render.sh
+++ render.sh
@@ -7,4 +7,6 @@ tsvfile=$here/target/${datestr}.tsv
 output=$here/target/${datestr}.png
 cat $here/logs/speed_${datestr}.log | awk -F'[ /:]' '{print $1"/"$2"/"$3" "$4":"$5":"$6"\t"$15}' > $tsvfile
 gnuplot -e "datafile='${tsvfile}'; outputfile='${output}'" $here/render.gp
+
+# configure as you like
 slackcat -c room_nyango $output
